5aebb509d84fc205954ad76f83578ee537529531
Michael Gutteridge <mrg@fhcrc.org>
2016-08-25 15:21:26 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# hutchbase-gw
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'hutchbase-gw'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures hutchbase-gw'
+long_description 'Installs/Configures hutchbase-gw'
+version '0.1.0'
