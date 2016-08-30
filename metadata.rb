e4ce5b4b31fb327ee356690e1e35f03cdbcae3d7
Michael Gutteridge <mrg@fredhutch.org>
2016-08-30 05:25:35 -0700

    added certificate and cert tests

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,4 @@ long_description 'Installs/Configures hutchbase-gw'
 version '0.1.0'
 
 depends 'nginx', '~> 2.7.9'
+depends 'chef-vault'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,44 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 #
+
+# Deploy ssl certs from vault
+
+include_recipe 'chef-vault::default'
+
+ssl_cert = chef_vault_item('hutchbase-gw', 'cert')['content']
+ssl_cert_key = chef_vault_item('hutchbase-gw', 'key')['content']
+
+etcdir = '/etc/hutchbase-gw'
+certfile = "#{etcdir}/#{node['hutchbase-gw']['ssl_cert']}"
+keyfile = "#{etcdir}/#{node['hutchbase-gw']['ssl_cert_key']}"
+
+directory node.default['hutchbase-gw']['logdir'] do
+    owner 'root'
+    group 'root'
+    mode '0755'
+end
+
+directory etcdir do
+    owner 'root'
+    group 'root'
+    mode '0755'
+end
+
+file certfile do
+    content ssl_cert
+    owner 'root'
+    group 'root'
+    mode '0644'
+end
+
+file keyfile do
+    content ssl_cert_key
+    owner 'root'
+    group 'root'
+    mode '0644'
+end
+
 include_recipe 'nginx'
 
 nginx_site 'hutchbase-gw' do
@@ -11,8 +49,8 @@ nginx_site 'hutchbase-gw' do
   template 'hutchbase.conf.erb'
   variables(
     'server_name' => node['hutchbase-gw']['server_name'],
-    'ssl_cert' => node['hutchbase-gw']['ssl_cert'],
-    'ssl_cert_key' => node['hutchbase-gw']['ssl_cert_key'],
+    'ssl_cert' => certfile,
+    'ssl_cert_key' => keyfile,
     'logdir' => node['hutchbase-gw']['logdir'],
     'uri' => "#{node['hutchbase-gw']['url']}:#{node['hutchbase-gw']['port']}"
   )
