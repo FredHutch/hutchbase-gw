1d6dfb27bb3fa95925bab6b3596152c344bddbd2
Michael Gutteridge <mrg@fhcrc.org>
2016-11-23 10:16:39 -0800

    Add application deploy
    Deploy application from artifact server via `remote_file` resource.

--- attributes/hutchbase_gw.rb
+++ attributes/hutchbase_gw.rb
@@ -5,3 +5,8 @@ node.default['hutchbase-gw']['url'] = 'http://localhost'
 node.default['hutchbase-gw']['port'] = '8080'
 node.default['hutchbase-gw']['ssl_cert'] = 'ssl.cert'
 node.default['hutchbase-gw']['ssl_cert_key'] = 'ssl.cert.key'
+node.default['hutchbase-gw']['source'] = {
+  'url' => 'http://octopus.fhcrc.org/misc/hutchbaseExternal.war',
+  'checksum' => 'd35aaff5aceca47345c148ec7390de3f'\
+                '1ff81b8fa27839165867788170bc2b7c'
+}
