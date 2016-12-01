c67c3a304a2f22d5fb44597f16f283a0ef91a03e
Michael Gutteridge <mrg@fhcrc.org>
2016-12-01 09:21:34 -0800

    added sample app for testing

--- recipes/default.rb
+++ recipes/default.rb
@@ -72,16 +72,20 @@ tomcat_service 'hutchbase-min' do
   action :start
 end
 
+remote_file '/opt/tomcat_hutchbase-min/webapps/sample.war' do
+  owner 'tomcat_hutchbase-min'
+  group 'tomcat_hutchbase-min'
+  mode '0644'
+  source 'https://tomcat.apache.org/tomcat-6.0-doc/appdev/sample/sample.war'
+  checksum node['hutchbase-gw']['source']['checksum']
+  notifies :restart, 'tomcat_service[hutchbase-min]', :delayed
+end
+
 remote_file '/opt/tomcat_hutchbase-min/webapps/hutchbaseExternal.war' do
   owner 'tomcat_hutchbase-min'
   group 'tomcat_hutchbase-min'
   mode '0644'
   source node['hutchbase-gw']['source']['url']
   checksum node['hutchbase-gw']['source']['checksum']
-  notifies :restart, 'tomcat_service[hutchbase-min]'
+  notifies :restart, 'tomcat_service[hutchbase-min]', :delayed
 end
-node.default['hutchbase-gw']['source'] = {
-  'url' => 'http://octopus.fhcrc.org/misc/hutchbaseExternal.war',
-  'checksum' => 'd35aaff5aceca47345c148ec7390de3f'\
-                '1ff81b8fa27839165867788170bc2b7c'
-}
